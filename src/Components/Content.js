6783cfa207374f727a0ff06104a6b17cf78c7bf2
irinapizane <90336924+irinapizane@users.noreply.github.com>
2022-02-13 00:18:40 +0200

    updated category1

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,3 +1,4 @@
+
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import Category1 from "../Pages/Category1"
--- src/Pages/Category1.js
+++ src/Pages/Category1.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ProductCardTop from '../Components/ProductCardTop'
 import ProductCardBottom from '../Components/ProductCardBottom'
 
+
 export default function Category1() {
     return (
-        <div className="ms-5">
+        <div className="category-page ms-5">
             <ProductCardTop />
             <ProductCardBottom />
         </div>
