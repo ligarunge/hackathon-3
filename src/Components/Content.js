4aaa7d8612cb2c61ffe98511f519252dd52e8e56
ligarunge <liga.runge@gmail.com>
2022-02-12 10:22:00 +0200

    Initial commit

---
+++ src/Components/Content.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+function Content() {
+    return (
+        <div>
+<Routes>
+    <Route path="/" element={}/>
+    <Route path="/" element={}/>
+    
+</Routes>
+        </div>
+    )
+}
+export default Content;
