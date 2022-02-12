8a0186c273459e2b9a6ad3d5d14b0a4400e14b89
Baibse <84922086+Baibse@users.noreply.github.com>
2022-02-12 12:10:12 +0200

    Header_updated with logo

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home";
 
 function Content() {
     return (
         <div>
 <Routes>
-    <Route path="/" element={}/>
-    <Route path="/" element={}/>
+    <Route path="/" element={<Home />}/>
+    {/* <Route path="/" element={}/> */}
     
 </Routes>
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom"
+import "./Header.css"
+
 function Header() {
     return (
         <div>
-
+            <nav className="navbar navbar-light bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-white" href="#">
+                        <img src="/images/logo_noBackground.png" alt="" width="35" height="40" className="d-inline-block align-text-center" />
+                        PearTech
+                    </a>
+                    <ul className="nav justify-content-center">
+                        <li className="nav-item">
+                            <Link className="nav-link active text-white" to='/'>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-secondary" to='/'>Categories</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link text-secondary" to='/'>Cart</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
