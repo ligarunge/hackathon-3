b8ebc4a1e45bbf610270e27dffeb04b17037a995
laimav <90565579+laimav@users.noreply.github.com>
2022-02-13 22:06:11 +0200

    Cart update

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -5,21 +5,32 @@ import CartSummaryTab from "../Components/CartSummaryTab";
 
 function Cart() {
     return (
-        <div className="container container-height">
+       <div> 
+           <div className="mt-0 mb-4 top-title">
+           <div className="container">
+                <div className="row pt-1">
+                    <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item"><a className="link-none" href="/">Home</a></li>
+                            <li className="breadcrumb-item active" aria-current="page">Shopping Cart</li>
+                        </ol>
+                    </nav>
+                </div>
+                </div>
+        <p className="display-5 container">Shopping Cart</p>
+    </div>
 
-            <div className="col mt-3 mb-4">
-                <h2>Cart</h2>
-            </div>
+<div className="container container-height container-cart">
 
             <ul className="nav nav-tabs" id="myTab" role="tablist">
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link active fw-bold" id="products-tab" data-bs-toggle="tab" data-bs-target="#products" type="button" role="tab" aria-controls="home" aria-selected="true">Products</button>
+                    <button className="nav-link active" id="products-tab" data-bs-toggle="tab" data-bs-target="#products" type="button" role="tab" aria-controls="home" aria-selected="true"><h6>Products</h6></button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link fw-bold" id="address-tab" data-bs-toggle="tab" data-bs-target="#address" type="button" role="tab" aria-controls="profile" aria-selected="false">Address</button>
+                    <button className="nav-link fw-bold" id="address-tab" data-bs-toggle="tab" data-bs-target="#address" type="button" role="tab" aria-controls="profile" aria-selected="false"><h6>Address</h6></button>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <button className="nav-link fw-bold" id="summary-tab" data-bs-toggle="tab" data-bs-target="#summary" type="button" role="tab" aria-controls="contact" aria-selected="false">Summary</button>
+                    <button className="nav-link fw-bold" id="summary-tab" data-bs-toggle="tab" data-bs-target="#summary" type="button" role="tab" aria-controls="contact" aria-selected="false"><h6>Summary</h6></button>
                 </li>
             </ul>
             <div className="tab-content" id="myTabContent">
@@ -30,9 +41,10 @@ function Cart() {
 
             <div className="row mt-3 mb-3 mx-1">
                 <div className="col text-end">
-                    <h4>Total: <span id="total3">179.97</span> EUR</h4>
+                    <h4>Total: 179.97 EUR</h4>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
