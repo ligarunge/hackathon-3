b5478d759ed2b892c9342850f2a2133a2dd8f4e7
irinapizane <90336924+irinapizane@users.noreply.github.com>
2022-02-13 15:34:34 +0200

    card link update

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -8,16 +8,18 @@ export default function ProductCard(props) {
     }
     
     return (
-        <div className="card">
-            {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`${props.item.coverImg}`} className="card--image" />
-            <div className="card--stats">
-                <img src="/images/star-red.png" className="card--star" />
-                <span>{props.item.stats.rating} </span>
-                <span className="gray">({props.item.stats.reviewCount}) </span>
+        <a href="product" className="product-card-link">
+            <div className="card">
+                {badgeText && <div className="card--badge">{badgeText}</div>}
+                <img src={`${props.item.coverImg}`} className="card--image" />
+                <div className="card--stats">
+                    <img src="/images/star-red.png" className="card--star" />
+                    <span>{props.item.stats.rating} </span>
+                    <span className="gray">({props.item.stats.reviewCount}) </span>
+                </div>
+                <p className="card--title">{props.item.title}</p>
+                <p className="card--price"><span className="bold"> ${props.item.price}</span></p>
             </div>
-            <p className="card--title">{props.item.title}</p>
-            <p className="card--price"><span className="bold"> ${props.item.price}</span></p>
-        </div>
+        </a>
     )
 }
