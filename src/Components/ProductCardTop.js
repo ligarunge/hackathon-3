5e6207583e57118cf67554c9beec3f7fd4a6abec
ligarunge <liga.runge@gmail.com>
2022-02-14 09:13:40 +0200

    text align

--- src/Components/ProductCardTop.js
+++ src/Components/ProductCardTop.js
@@ -20,7 +20,7 @@ export default function ProductCardTop() {
            
             <div className="category-top mt-5 pb-5">
                 <img src="/images/product_images/mouse1-img-3.png" alt="mouse" className="category-image-left"/>
-                <div className="category-text ms-5">
+                <div className="category-text ms-5" style={{textAlign: 'justify'}}>
                     <p>Shop wireless and wired mice. Choose from the Bluetooth®, performance, ergonomic, business, EDU, travel, compact, and WFH product range. </p>
                     <p>Founded in 1981 in Lausanne, Switzerland, and quickly expanding to the Silicon Valley, Logitech started connecting people through innovative computer peripherals and many industry firsts, including the infrared cordless mouse, the thumb-operated trackball, the laser mouse, and more.</p>
                 </div>
