db4f219d3573873f214fdc5e72420a35e416f4c6
ligarunge <liga.runge@gmail.com>
2022-02-14 08:37:58 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,8 @@
-import CategoryItem from "../Components/CategoryItem";
+
 import Footer from "../Components/Footer";
 import Carousel from "./Carousel";
 import categories from "../Data/categories";
-import CategoryItemReversed from "../Components/CategoryItemReversed";
+import CategoryItemHome from "../Components/CategoryItemHome";
 
 
 function Home() {
@@ -13,19 +13,16 @@ function Home() {
       <div className="container">
           <div className="display-5 my-4">Categories</div>
           <div className="row">
-            <div className="col">
-          <CategoryItem name={categories[0].name} description={categories[0].description} imageUrl={categories[0].imageUrl}/>
+                <div className="col-md-4">
+                <CategoryItemHome name={categories[0].name} description={categories[0].description} imageUrl={categories[0].imageUrl}/>
+                </div>
+                <div className="col-md-4">
+                <CategoryItemHome name={categories[1].name} description={categories[1].description} imageUrl={categories[1].imageUrl}/>    
+                </div>
+                <div className="col-md-4">
+                <CategoryItemHome name={categories[2].name} description={categories[2].description} imageUrl={categories[2].imageUrl}/> 
+                </div>
             </div>
-            <div className="col">
-          <CategoryItem name={categories[0].name} description={categories[0].description} imageUrl={categories[0].imageUrl}/>
-            </div>
-            <div className="col">
-          <CategoryItem name={categories[0].name} description={categories[0].description} imageUrl={categories[0].imageUrl}/>
-            </div>
-          </div>
-          {/* <CategoryItemReversed name={categories[1].name} description={categories[1].description} imageUrl={categories[1].imageUrl}/>
-          <CategoryItem name={categories[2].name} description={categories[2].description} imageUrl={categories[2].imageUrl}/>    */}
-
       </div>
 
       </div>
