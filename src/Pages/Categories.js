c901a519f709c63f2d7253a2cf6967c990f03107
ligarunge <liga.runge@gmail.com>
2022-02-14 09:09:29 +0200

    header removed

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -46,17 +46,7 @@ function Categories() {
             <CategoryItemReversed name={categories[1].name} description={categories[1].description} imageUrl={categories[1].imageUrl}/>
             <CategoryItem name={categories[2].name} description={categories[2].description} imageUrl={categories[2].imageUrl}/>   
             
-            <div className="row">
-                <div className="col-md-4">
-                <CategoryItemHome name={categories[0].name} description={categories[0].description} imageUrl={categories[0].imageUrl}/>
-                </div>
-                <div className="col-md-4">
-                <CategoryItemHome name={categories[1].name} description={categories[1].description} imageUrl={categories[1].imageUrl}/>    
-                </div>
-                <div className="col-md-4">
-                <CategoryItemHome name={categories[2].name} description={categories[2].description} imageUrl={categories[2].imageUrl}/> 
-                </div>
-            </div>
+           
             </div>
         </div>
     </div>
